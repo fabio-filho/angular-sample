e5ab7bd02a81a2e91f9d178ca739f7719820ae6e
Fábio Filho <fabio.filhossi@gmail.com>
2017-09-01 18:18:08 -0300

    Setting some directives!

--- app/controller/book/booksController.js
+++ app/controller/book/booksController.js
@@ -1,18 +1,37 @@
-(function(){
+(function () {
 
-    let booksController = function($scope){
-        this.message = "Hello World!";       
+    let booksController = function ($scope) {
+        this.title = "Just a sample app";
 
         $scope.books = [];
 
         $scope.fetchBooks = function () {
             $scope.books = [
                 { ID: 1, BookName: "Test Books 1", AuthorName: "Test Author 1", ISBN: "TEST1" },
-                { ID: 2, BookName: "Test Books 2", AuthorName: "Test Author 2", ISBN: "TEST2"},
-                { ID: 3, BookName: "Test Books 3", AuthorName: "Test Author 3", ISBN: "TEST3"},
-                { ID: 4, BookName: "Test Books 4", AuthorName: "Test Author 4", ISBN: "TEST4"},
-                { ID: 5, BookName: "Test Books 5", AuthorName: "Test Author 5", ISBN: "TEST5"}
+                { ID: 2, BookName: "Test Books 2", AuthorName: "Test Author 2", ISBN: "TEST2" },
+                { ID: 3, BookName: "Test Books 3", AuthorName: "Test Author 3", ISBN: "TEST3" },
+                { ID: 4, BookName: "Test Books 4", AuthorName: "Test Author 4", ISBN: "TEST4" },
+                { ID: 5, BookName: "Test Books 5", AuthorName: "Test Author 5", ISBN: "TEST5" }
             ];
+        }
+
+        $scope.country = null;
+
+        $scope.countries = [
+            "India",
+            "Denmark",
+            "USA",
+            "Singapore",
+            "Germany"
+        ];
+
+        $scope.getStyle = function (index) {
+            if (index == 1)
+                return { 'color': 'red' };
+            if (index == 2)
+                return { 'color': 'blue' };
+            if (index == 3)
+                return { 'color': 'green' };
         }
     }
     angular.module('myAngularApplication')
