6aaa0dafd67ad40366de69b7f4f75b8b2530110b
Fábio Filho <fabio.filhossi@gmail.com>
2017-09-04 15:55:11 -0300

    Fixing the last commit.

--- app/application/app.js
+++ app/application/app.js
@@ -1,13 +1,3 @@
 (function(){
     let myModule = angular.module('myAngularApplication', []);
-
-    myModule.filter('startFrom', function() {
-	return function(input, start) {
-		if (input) {
-			start = +start;	// parse to int
-			return input.slice(start);
-		}
-		return [];
-	}
-});
 }())
