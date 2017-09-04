54a377bc63a1b07c27f2839da82908885c5007cb
Fábio Filho <fabio.filhossi@gmail.com>
2017-09-04 12:33:04 -0300

    Creating a module - part II

--- app/controller/book/booksController.js
+++ app/controller/book/booksController.js
@@ -1,17 +1,27 @@
 (function () {
 
     let booksController = function ($scope) {
-        this.title = "Just a sample app";
+        this.title = "Books";
 
         $scope.books = [];
+        $scope.amountBooksVisible = 5;
+        $scope.limitRowsPerPage = 5;
 
         $scope.fetchBooks = function () {
             $scope.books = [
-                { ID: 1, BookName: "Test Books 1", AuthorName: "Test Author 1", ISBN: "TEST1" },
-                { ID: 2, BookName: "Test Books 2", AuthorName: "Test Author 2", ISBN: "TEST2" },
-                { ID: 3, BookName: "Test Books 3", AuthorName: "Test Author 3", ISBN: "TEST3" },
-                { ID: 4, BookName: "Test Books 4", AuthorName: "Test Author 4", ISBN: "TEST4" },
-                { ID: 5, BookName: "Test Books 5", AuthorName: "Test Author 5", ISBN: "TEST5" }
+                { ID: 1, BookName: "Test Books 1", AuthorName: "Test Author 1", ISBN: "TEST1", DateReleased:1288323623006 },
+                { ID: 2, BookName: "Test Books 2", AuthorName: "Test Author 2", ISBN: "TEST2", DateReleased:1288323623006 },
+                { ID: 3, BookName: "Test Books 3", AuthorName: "Test Author 3", ISBN: "TEST3", DateReleased:1288323623006 },
+                { ID: 4, BookName: "Test Books 4", AuthorName: "Test Author 4", ISBN: "TEST4", DateReleased:1288323623006 },
+                { ID: 5, BookName: "Test Books 5", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 6, BookName: "Test Books 6", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 7, BookName: "Test Books 7", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 8, BookName: "Test Books 8", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 9, BookName: "Test Books 9", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 10, BookName: "Test Books 10", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 11, BookName: "Test Books 11", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 12, BookName: "Test Books 12", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
+                { ID: 13, BookName: "Test Books 13", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 }                
             ];
         }
 
@@ -25,7 +35,7 @@
             "Germany"
         ];
 
-        $scope.getStyle = function (index) {
+        $scope.getStyle = function (index) {            
             if (index == 1)
                 return { 'color': 'red' };
             if (index == 2)
