c4ffbdad843ce18721280755a287207587e49fae
Fábio Filho <fabio.filhossi@gmail.com>
2017-09-04 15:52:38 -0300

    Creating a module - part III

--- app/application/app.js
+++ app/application/app.js
@@ -1,3 +1,13 @@
 (function(){
     let myModule = angular.module('myAngularApplication', []);
+
+    myModule.filter('startFrom', function() {
+	return function(input, start) {
+		if (input) {
+			start = +start;	// parse to int
+			return input.slice(start);
+		}
+		return [];
+	}
+});
 }())
--- app/controller/book/booksController.js
+++ app/controller/book/booksController.js
@@ -4,24 +4,26 @@
         this.title = "Books";
 
         $scope.books = [];
-        $scope.amountBooksVisible = 5;
+        $scope.currentIndex = 0;
         $scope.limitRowsPerPage = 5;
+        $scope.fields = ['ID', 'BookName', 'AuthorName', 'DateReleased'];
+        $scope.orderField = $scope.fields[0];
 
         $scope.fetchBooks = function () {
             $scope.books = [
-                { ID: 1, BookName: "Test Books 1", AuthorName: "Test Author 1", ISBN: "TEST1", DateReleased:1288323623006 },
-                { ID: 2, BookName: "Test Books 2", AuthorName: "Test Author 2", ISBN: "TEST2", DateReleased:1288323623006 },
-                { ID: 3, BookName: "Test Books 3", AuthorName: "Test Author 3", ISBN: "TEST3", DateReleased:1288323623006 },
-                { ID: 4, BookName: "Test Books 4", AuthorName: "Test Author 4", ISBN: "TEST4", DateReleased:1288323623006 },
-                { ID: 5, BookName: "Test Books 5", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 6, BookName: "Test Books 6", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 7, BookName: "Test Books 7", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 8, BookName: "Test Books 8", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 9, BookName: "Test Books 9", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 10, BookName: "Test Books 10", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 11, BookName: "Test Books 11", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 12, BookName: "Test Books 12", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 },
-                { ID: 13, BookName: "Test Books 13", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased:1288323623006 }                
+                { ID: 1, BookName: "Test Books 1", AuthorName: "Test Author 1", ISBN: "TEST1", DateReleased: 786585600000 },
+                { ID: 5, BookName: "Test Books 5", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 983923200000 },
+                { ID: 3, BookName: "Test Books 3", AuthorName: "Test Author 3", ISBN: "TEST3", DateReleased: -1720051200000 },
+                { ID: 2, BookName: "Test Books 2", AuthorName: "Test Author 2", ISBN: "TEST2", DateReleased: 1288323623006 },
+                { ID: 4, BookName: "Test Books 4", AuthorName: "Test Author 4", ISBN: "TEST4", DateReleased: 1288323723006 },
+                { ID: 6, BookName: "Test Books 6", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 947116800000 },
+                { ID: 13, BookName: "Test Books 13", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 1288313623006 },
+                { ID: 7, BookName: "Test Books 7", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 1288323623006 },
+                { ID: 8, BookName: "Test Books 8", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: -454204800000 },
+                { ID: 9, BookName: "Test Books 9", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 1288323623006 },
+                { ID: 11, BookName: "Test Books 11", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 1228323623006 },
+                { ID: 10, BookName: "Test Books 10", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 1288313121006 },
+                { ID: 12, BookName: "Test Books 12", AuthorName: "Test Author 5", ISBN: "TEST5", DateReleased: 1118323623006 }
             ];
         }
 
@@ -35,13 +37,26 @@
             "Germany"
         ];
 
-        $scope.getStyle = function (index) {            
+        $scope.getStyle = function (index) {
             if (index == 1)
                 return { 'color': 'red' };
             if (index == 2)
                 return { 'color': 'blue' };
             if (index == 3)
                 return { 'color': 'green' };
+        }
+
+        $scope.nextPage = function () {
+            $scope.currentIndex += $scope.limitRowsPerPage;
+        }
+
+        $scope.previousPage = function () {
+            $scope.currentIndex -= $scope.limitRowsPerPage;
+        }
+
+        $scope.isLimitOfPage = function () {
+            return ($scope.currentIndex+$scope.limitRowsPerPage) / $scope.limitRowsPerPage
+                == Math.ceil($scope.books.length / $scope.limitRowsPerPage);
         }
     }
     angular.module('myAngularApplication')
